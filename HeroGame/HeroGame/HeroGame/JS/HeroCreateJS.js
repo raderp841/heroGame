32d0e904d4df3f898503435a3cb249e570414a09
Peter Rader <raderp841@gmail.com>
2018-03-10 17:53:27 -0500

    added jquery to show/hide heroes and inventories

--- HeroGame/HeroGame/HeroGame/JS/HeroCreateJS.js
+++ HeroGame/HeroGame/HeroGame/JS/HeroCreateJS.js
@@ -5,14 +5,46 @@ $(document).ready(function () {
     var heroForm = $('#heroForm');
     var newHeroButton = $('#newHero');
     var heroList = $('#heroList');
+    var heroes = $('.heroClass');
+    var inventories = $('.inventoryClass');
+    var viewInventory = $('.viewInventoryButton');
+    var viewHeroButton = $('.viewHeroButton');
+
 
     heroForm.hide();
+    inventories.hide();
 
     newHeroButton.click(function () {
         heroForm.show();
         newHeroButton.hide();
         heroList.hide();
     });
+
+    viewInventory.click(function () {
+        var button = $(this);
+        var parentElement = button.parent();
+        var inventoryElement = parentElement.next();
+
+        parentElement.hide();
+        inventoryElement.show();
+    });
+
+    viewHeroButton.click(function () {
+        var button = $(this);
+        var parentElement = button.parent();
+        var heroElement = parentElement.prev();
+
+        parentElement.hide();
+        heroElement.show();
+    });
+
+    
+
+    
+
+
+
+
 
 
 
