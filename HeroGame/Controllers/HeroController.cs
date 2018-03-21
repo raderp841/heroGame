89afaab47a2dbe40aeda9340d49898e74bde986e
Josh Tucholski <josh@techelevator.com>
2018-03-20 21:58:08 -0400

    Removed Layout Pages from InGame view.

---
+++ HeroGame/Controllers/HeroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HeroGame.Controllers
+{
+    public class HeroController : Controller
+    {
+        // GET: Hero
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
